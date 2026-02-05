6dca86b66185e0ced8428c60d4e0e6729bc33409
Jimi <moseley.jamie@gmail.com>
2026-02-05 14:39:02 -0600

    Update rotationMatrixToZYZ.m
    Fixed bounds on if statement, done on iPad and unable to test code until I get home.

--- rotationMatrixToZYZ.m
+++ rotationMatrixToZYZ.m
@@ -12,7 +12,7 @@ function eul = rotationMatrixToZYZ(R)
 
     psi = atan2(R(3,2), -R(3,1));
 
-    if theta > pi/2
+    if theta > pi/2 || theta < 0
         phi = atan2(-R(2,3),-R(1,3));
 
         theta = atan2(-sqrt(R(1,3)^2 + R(2,3)^2),R(3,3));
